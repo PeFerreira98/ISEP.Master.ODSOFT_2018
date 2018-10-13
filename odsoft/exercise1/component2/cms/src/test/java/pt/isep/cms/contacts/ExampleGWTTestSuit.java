ee25df8582d4f52c9ea6bafb764a656df62d888a
PeFerreira98 <peferreira98@outlook.com>
2018-10-13 21:54:47 +0100

    Little changes to cms

--- odsoft/exercise1/component2/cms/src/test/java/pt/isep/cms/contacts/ExampleGWTTestSuit.java
+++ odsoft/exercise1/component2/cms/src/test/java/pt/isep/cms/contacts/ExampleGWTTestSuit.java
@@ -7,9 +7,9 @@ import junit.framework.TestSuite;
 import pt.isep.cms.contacts.client.ExampleGWTTest;
 
 public class ExampleGWTTestSuit extends GWTTestSuite {
-	  public static Test suite() {
-		    TestSuite suite = new TestSuite("Test for the Contacts Application");
-		    suite.addTestSuite(ExampleGWTTest.class); 
-		    return suite;
-		  }
-		} 
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for the Contacts Application");
+		suite.addTestSuite(ExampleGWTTest.class);
+		return suite;
+	}
+}
