1343787273838a5b6f5e6981be644098c79cbcc1
PeFerreira98 <peferreira98@outlook.com>
2018-10-18 22:59:01 +0100

    Add students base + sonarqube support (extra feature) issue #1

---
+++ odsoft/exercise1/component2/cms/src/main/java/pt/isep/cms/students/server/StudentsServiceImpl.java
@@ -0,0 +1,82 @@
+package pt.isep.cms.students.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import pt.isep.cms.students.client.StudentsService;
+import pt.isep.cms.students.shared.Student;
+import pt.isep.cms.students.shared.StudentDetails;
+
+@SuppressWarnings("serial")
+public class StudentsServiceImpl extends RemoteServiceServlet implements
+    StudentsService {
+
+  private static final String[] studentsFirstNameData = new String[] {
+      "Bino", "Banana", "Tom"};
+  
+  private final String[] studentsLastNameData = new String[] {
+      "Binario", "Split", "Dicks"};
+  
+  private final String[] studentsEmailData = new String[] {
+      "bb@example.com", "bs@example.com", "td@example.com"};
+      
+  private final HashMap<String, Student> students = new HashMap<String, Student>();
+
+  public StudentsServiceImpl() {
+    initStudents();
+  }
+  
+  private void initStudents() {
+    // TODO: Create a real UID for each student
+    //
+    for (int i = 0; i < studentsFirstNameData.length && i < studentsLastNameData.length && i < studentsEmailData.length; ++i) {
+      Student student = new Student(String.valueOf(i), studentsFirstNameData[i], studentsLastNameData[i], studentsEmailData[i]);
+      students.put(student.getId(), student); 
+    }
+  }
+  
+  public Student addStudent(Student student) {
+    student.setId(String.valueOf(students.size()));
+    students.put(student.getId(), student); 
+    return student;
+  }
+
+  public Student updateStudent(Student student) {
+	  String lid=student.getId();
+    students.remove(student.getId());
+    students.put(student.getId(), student); 
+    return student;
+  }
+
+  public Boolean deleteStudent(String id) {
+    students.remove(id);
+    return true;
+  }
+  
+  public ArrayList<StudentDetails> deleteStudents(ArrayList<String> ids) {
+
+    for (int i = 0; i < ids.size(); ++i) {
+      deleteStudent(ids.get(i));
+    }
+    
+    return getStudentDetails();
+  }
+  
+  public ArrayList<StudentDetails> getStudentDetails() {
+    ArrayList<StudentDetails> studentDetails = new ArrayList<StudentDetails>();
+    
+    Iterator<String> it = students.keySet().iterator();
+    while(it.hasNext()) { 
+      Student student = students.get(it.next());          
+      studentDetails.add(student.getLightWeightStudent());
+    }
+    
+    return studentDetails;
+  }
+
+  public Student getStudent(String id) {
+    return students.get(id);
+  }
+}
