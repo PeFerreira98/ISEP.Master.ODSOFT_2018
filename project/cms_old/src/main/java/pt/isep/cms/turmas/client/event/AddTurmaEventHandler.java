86d38cf0131f40c9ac8f54cf1404fee6f1fa9dd4
rafael95 <1141233@isep.ipp.pt>
2018-12-22 21:02:51 +0000

    deleted old folder

--- project/cms_old/src/main/java/pt/isep/cms/turmas/client/event/AddTurmaEventHandler.java
+++
@@ -1,7 +0,0 @@
-package pt.isep.cms.turmas.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface AddTurmaEventHandler extends EventHandler {
-    void onAddTurma(AddTurmaEvent event);
-}
